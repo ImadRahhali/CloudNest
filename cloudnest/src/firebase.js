268408036e1d758ecaf3b20c8e121fd0dbc39de1
imadrahhali <imadrahhali5@gmail.com>
2024-04-09 15:10:04 +0000

    firebase config

---
+++ cloudnest/src/firebase.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCF5DSOkQ8wiOoR2gEr2gNjKJdjC_mquf0",
+  authDomain: "cloudnest-1.firebaseapp.com",
+  projectId: "cloudnest-1",
+  storageBucket: "cloudnest-1.appspot.com",
+  messagingSenderId: "331574939803",
+  appId: "1:331574939803:web:51d0f1e734bab25426363e",
+  measurementId: "G-79FMX4TFB0"
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
