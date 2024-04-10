34cb0e0b74dbcc86aab162889676338e9cb28474
imadrahhali <imadrahhali5@gmail.com>
2024-04-10 16:33:17 +0000

    landing page v1

---
+++ cloudnest/src/Components/Hero.jsx
@@ -0,0 +1,46 @@
+import styles from "../style";
+import { discount, robot } from "../assets";
+import GetStarted from "./GetStarted";
+
+const Hero = () => {
+  return (
+    <section id="home" className={`flex md:flex-row flex-col ${styles.paddingY}`}>
+      <div className={`flex-1 ${styles.flexStart} flex-col xl:px-0 sm:px-16 px-6`}>
+
+
+        <div className="flex flex-row justify-between items-center w-full">
+          <h1 className="flex-1 font-poppins font-semibold ss:text-[72px] text-[52px] text-white ss:leading-[100.8px] leading-[75px]">
+          Today With  <br className="sm:block hidden" />{" "}
+            <span className="text-gradient">CloudNest:</span>{" "}
+          </h1>
+          <div className="ss:flex hidden md:mr-4 mr-0">
+            <GetStarted />
+          </div>
+        </div>
+
+        <h1 className="font-poppins font-semibold ss:text-[68px] text-[52px] text-white ss:leading-[100.8px] leading-[75px] w-full">
+        Elevate Your Data Experience.
+        </h1>
+        <p className={`${styles.paragraph} max-w-[470px] mt-5`}>
+        We offer you  cutting-edge cloud storage and file management solutions tailored for modern data needs. Experience secure, seamless data handling and collaboration with CloudNest's advanced platform
+        </p>
+      </div>
+
+      <div className={`flex-1 flex ${styles.flexCenter} md:my-0 my-10 relative`}>
+        <img src={robot} alt="billing" className="w-[110%] h-[110%] relative z-[5]" style={{ top: "-50px" }} />
+
+        {/* gradient start */}
+        <div className="absolute z-[0] w-[40%] h-[35%] top-0 pink__gradient" />
+        <div className="absolute z-[1] w-[80%] h-[80%] rounded-full white__gradient bottom-40" />
+        <div className="absolute z-[0] w-[50%] h-[50%] right-20 bottom-20 blue__gradient" />
+        {/* gradient end */}
+      </div>
+
+      <div className={`ss:hidden ${styles.flexCenter}`}>
+        <GetStarted />
+      </div>
+    </section>
+  );
+};
+
+export default Hero;
---
+++ cloudnest/tailwind.config.js
@@ -0,0 +1,27 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
+  mode: "jit",
+  theme: {
+    extend: {
+      colors: {
+        primary: "#000000",
+        secondary: "#0f1042",
+        dimWhite: "#FFFFFF",
+        dimBlue: "#05081c",
+      },
+      fontFamily: {
+        poppins: ["Poppins", "sans-serif"],
+      },
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+};
