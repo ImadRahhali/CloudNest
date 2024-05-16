00ef54e7524b9749bfc22e7a754ffbc4188b5817
TahirRida <tahirrida52@gmail.com>
2024-05-16 20:15:34 +0100

    Preparing to merge

--- cloudnest/src/Components/MainPageComponents/SideBar/Sidebar.jsx
+++ cloudnest/src/Components/MainPageComponents/SideBar/Sidebar.jsx
@@ -4,6 +4,7 @@ import "./Sidebar.css";
 import logo from "../../../assets/CloudNestLogo.png";
 import { auth } from "../../../firebase";
 import { Avatar } from "@mui/material";
+import { Link, useNavigate } from "react-router-dom";
 const Sidebar = ({
   openFileUpload,
   closFileModal,
@@ -11,23 +12,39 @@ const Sidebar = ({
   closeCreateFolder,
   shouldRerender,
 }) => {
-  console.log(auth.currentUser);
+  const navigate = useNavigate();
+  const Logout = async () => {
+    try {
+      await auth.signOut();
+      navigate("/"); // Redirect to the login or landing page after logout
+    } catch (error) {
+      console.error("Error logging out:", error);
+    }
+  };
   return (
     <div className="Side h-full w-full w-1/4 p-4 flex flex-col justify-between">
       {/* Home Section */}
       <div>
         <div className="mb-4">
-          <img src={logo} className="w-52 h-12 mb-2"></img>
-          <h2 className="flex ">
-            <Avatar
-              alt="User Avatar"
-              src={auth.currentUser.photoUrl}
-              sx={{ width: 60, height: 60 }}
-            />
-            <span className="flex justify-center items-center ml-2">
-              Hello {auth.currentUser.displayName}
-            </span>
-          </h2>
+          <Link to="/profile">
+            <img src={logo} className="w-52 h-12 mb-2"></img>
+            <h2 className="flex ">
+              <Avatar
+                alt="User Avatar"
+                src={auth.currentUser.photoUrl}
+                sx={{ width: 60, height: 60 }}
+              />
+              <span className="flex justify-center items-center ml-2">
+                Hello {auth.currentUser.displayName}
+              </span>
+            </h2>
+          </Link>
+          <button
+            className="text-white font-semibold py-2 px-4 rounded w-full mb-2"
+            onClick={Logout}
+          >
+            Logout
+          </button>
           <StoragePercentage shouldRerender={shouldRerender} />
           {/* Add links or other content for the home section if needed */}
         </div>
